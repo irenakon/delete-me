e9227acd98237e904afe31f15005e8e71cc1f9f0
eranb <levieb2@gmail.com>
2015-07-31 20:38:50 +0300

    ff

--- commands.c
+++ commands.c
@@ -369,7 +369,6 @@ bool commands_is_valid_addressing(int command, char *line) {
 //gets an argument from a line
 bool commands_get_one_argument(char *line, command_addressing addressing, int *data, int ic)
 {
-  puts("a");
 	int first_label_value;
 	int second_label_value;
 
@@ -381,7 +380,6 @@ bool commands_get_one_argument(char *line, command_addressing addressing, int *d
 	int i = 0;
 	line = trim_white_spaces(line);
 	strncpy(templine, line, strnlen(line, ASSEMBLER_LINE_SIZE));
-  printf("addressing: %d\n",addressing);
 	switch(addressing) {
 		case IMMEDIATE:
 			*data = atoi(line + 1);
@@ -400,11 +398,9 @@ bool commands_get_one_argument(char *line, command_addressing addressing, int *d
 			}
 			second=trim_white_spaces(second);
 			if(!assembler_get_symbol_value(templine+2, &first_label_value, ic)) {
-          puts("b");
 				return false;
 			}
 			if(!assembler_get_symbol_value(second, &second_label_value, ic)) {
-          puts("c");
 				return false;
 			}
 
@@ -440,7 +436,6 @@ bool commands_get_one_argument(char *line, command_addressing addressing, int *d
 			*data = registers_get_register_value(trim_white_spaces(line)) << 2;
 			return true;
 	}
-    puts("z");
 	return false;
 }
 
